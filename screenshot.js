e2c586e9e6efb70826d38face1d7832471c6a7f8
jamal1aarab <jamal.aarab.111@gmail.com>
2024-07-11 12:41:10 +0100

    focus manup added and dynamique img handeling added

--- screenshot.js
+++ screenshot.js
@@ -1,10 +1,42 @@
-function setScreenshotUrl(htmlElement,screenshotUrl) {
-    document.getElementById('html-target').innerHTML = htmlElement;
-    document.getElementById('img-target').src = screenshotUrl;
-  }
+function setScreenshotUrls(htmlElement, screenshotUrls, screenshotFocusedUrls) {
+  document.getElementById('html-target').innerHTML = htmlElement;
+
+  const container = document.querySelector('.container');
   
-  chrome.runtime.onMessage.addListener(function (request) {
-    if (request.msg === 'screenshot') {
-      setScreenshotUrl(request.data.htmlElement,request.data.screenshotUrl);
-    }
-  });
+
+
+  // Function to create and append an img element
+  function appendImage(src) {
+    const img = document.createElement('img');
+    img.src = src;
+    img.alt = 'Screenshot preview';
+    img.height = 480;
+    container.appendChild(img);
+  }
+
+  // Append the main screenshots
+  if (screenshotUrls && screenshotUrls.length > 0) {
+    appendImage(screenshotUrls[0]);
+  } else {
+    appendImage('white.png'); // Default image if no URLs provided
+  }
+
+  // Append focused screenshots
+  if (screenshotFocusedUrls && screenshotFocusedUrls.length > 0) {
+    appendImage(screenshotFocusedUrls[0]);
+  } else {
+    appendImage('white.png'); // Default image if no URLs provided
+  }
+
+  // Append additional screenshots dynamically
+  for (let i = 1; i < screenshotUrls.length && i < screenshotFocusedUrls.length; i++) {
+    appendImage(screenshotUrls[i]);
+    appendImage(screenshotFocusedUrls[i]);
+  }
+}
+
+chrome.runtime.onMessage.addListener(function (request) {
+  if (request.msg === 'screenshot') {
+    setScreenshotUrls(request.data.htmlElement, request.data.screenshotUrls, request.data.screenshotFocusedUrls);
+  }
+});
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,8 @@
 // service-worker.js
-
 console.log("Service worker running...");
 
-// Listen for clicks on the extension's action (popup) button
+// Listen for clicks on the extension's action button
 chrome.action.onClicked.addListener(async function () {
-
 
   console.log("Action button clicked");
 
@@ -19,37 +17,41 @@ chrome.action.onClicked.addListener(async function () {
 
   console.log("Capturing elements...");
 
+  const selector = `
+    a[href]
+  `;
 
-  const interactiveElements = await chrome.scripting.executeScript({
+  const interactiveElementsHtml = await chrome.scripting.executeScript({
     target: { tabId: tabId },
-    func: () => {
-      const selectors = `
-      a[href]
-        `;
-      const interactiveElements = document.querySelectorAll(selectors);
-      return Array.from(interactiveElements).map(el => el.outerHTML);
-    }
+    func: (selector) => {
+      const interactiveElements = document.querySelectorAll(selector);
+
+      function escapeHtml(html) {
+        return html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+      }
+
+      return Array.from(interactiveElements).map(el => escapeHtml(el.outerHTML));
+    },
+    args: [selector]
   });
 
   const interactiveElementsRects = await chrome.scripting.executeScript({
     target: { tabId: tabId },
-    func: () => {
-      const selectors = `
-      a[href]
-    `;
-      const interactiveElements = document.querySelectorAll(selectors);
+    func: (selector) => {
+      const interactiveElements = document.querySelectorAll(selector);
+      interactiveElements[0].blur();
       return Array.from(interactiveElements).map(el => {
         const rect = el.getBoundingClientRect();
         return {
-          top: rect.top + window.scrollY,
-          left: rect.left + window.scrollX,
+          top: rect.top,
+          left: rect.left,
           width: rect.width,
           height: rect.height
         };
       });
-    }
+    },
+    args: [selector]
   });
-
 
 
 
@@ -66,100 +68,94 @@ chrome.action.onClicked.addListener(async function () {
           }
         });
       });
-  
+
       const response = await fetch(preCut);
       const blob = await response.blob();
-  
+
       const imgBitmap = await createImageBitmap(blob);
       const canvas = new OffscreenCanvas(imgBitmap.width, imgBitmap.height);
       const ctx = canvas.getContext('2d');
       ctx.drawImage(imgBitmap, 0, 0);
-  
+
+      const padding = 10;
+
       const { top, left, width, height } = rect;
-      const cropWidth = Math.floor(width);
-      const cropHeight = Math.floor(height);
-      const cropX = Math.floor(left);
-      const cropY = Math.floor(top);
-  
+      const cropWidth = Math.floor(width) + 2 * padding;
+      const cropHeight = Math.floor(height) + 2 * padding;
+      const cropX = Math.floor(left) - padding;
+      const cropY = Math.floor(top) - padding;
+
       if (cropWidth <= 0 || cropHeight <= 0 || cropX < 0 || cropY < 0) {
         throw new Error('Invalid crop dimensions');
       }
-  
+
       const croppedCanvas = new OffscreenCanvas(cropWidth, cropHeight);
       const croppedCtx = croppedCanvas.getContext('2d');
       croppedCtx.drawImage(canvas, cropX, cropY, cropWidth, cropHeight, 0, 0, cropWidth, cropHeight);
-  
+
       const croppedBlob = await croppedCanvas.convertToBlob();
       const croppedDataUrl = await new Promise((resolve) => {
         const reader = new FileReader();
         reader.onload = () => resolve(reader.result);
         reader.readAsDataURL(croppedBlob);
       });
-  
+
       return croppedDataUrl;
-  
+
     } catch (error) {
       console.error('Error capturing or cropping screenshot:', error);
     }
   }
-  
 
-  // Get the focused element's bounding box
 
-  // const result = await chrome.scripting.executeScript({
-  //   target: { tabId: tabId },
-  //   func: () => {
-  //     const focusedElement = document.activeElement;
-  //     if (focusedElement && focusedElement !== document.body && focusedElement !== document.documentElement) {
-  //       const rect = focusedElement?.getBoundingClientRect() || null;
-  //       const padding = 5; // Adjust the padding value as needed
-  //       if (rect) {
-  //         const x = Math.max(Math.floor(rect.left) - padding, 0);
-  //         const y = Math.max(Math.floor(rect.top) - padding, 0);
-  //         const width = Math.min(Math.floor(rect.width) + 2 * padding, window.innerWidth - x);
-  //         const height = Math.min(Math.floor(rect.height) + 2 * padding, window.innerHeight - y);
-  //         return { x, y, width, height };
-  //       }
-  //       return null;
-  //     } else {
-  //       return "No element focused on";
-  //     }
-  //   }
-  // });
 
-  // const rect = result[0]?.result || { x: 0, y: 0, width: 400, height: 1000 };
-
-  // const screenshotUrl = await captureAndCropScreenshot(rect);
-
-  // Extract the first rectangle from the result
   const rectsArray = interactiveElementsRects[0]?.result || [];
-
-  if (rectsArray.length > 0) {
-    const firstRect = rectsArray[0];
-    console.log(firstRect);
-
-    if (firstRect && typeof firstRect.top === 'number' && typeof firstRect.left === 'number' &&
-      typeof firstRect.width === 'number' && typeof firstRect.height === 'number') {
-      const screenshotUrl = await captureAndCropScreenshot(firstRect);
-    } else {
-      console.error('Invalid rect object:', firstRect);
-    }
-  } else {
-    console.error('No rectangles found in the result.');
-  }
 
 
   const screenshotUrl = await captureAndCropScreenshot(rectsArray[0]);
 
+  const screenshotUrls = [screenshotUrl];
+
+
+
+
+  const interactiveElementsRectsFocused = await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: (selector) => {
+      const interactiveElements = document.querySelectorAll(selector);
+
+      interactiveElements[0].focus();
+      return Array.from(interactiveElements).map(el => {
+        const rect = el.getBoundingClientRect();
+        return {
+          top: rect.top,
+          left: rect.left,
+          width: rect.width,
+          height: rect.height
+        };
+      });
+    },
+    args: [selector]
+  });
+
+
+
+  const rectsArrayFocused = interactiveElementsRectsFocused[0]?.result || [];
+
+  const screenshotFocusedUrl = await captureAndCropScreenshot(rectsArrayFocused[0]);
+
+  const screenshotFocusedUrls = [screenshotFocusedUrl]
+
+
+
   //
 
-  const htmlElements = interactiveElements[0]?.result || "";
+  const htmlElements = interactiveElementsHtml[0]?.result || "";
   const htmlElement = htmlElements[0];
   console.log(htmlElement);
 
 
-  //
-
+  // Open the screenshot in a new tab
 
   const viewTabUrl = chrome.runtime.getURL('screenshot.html');
   let targetId = null;
@@ -168,7 +164,7 @@ chrome.action.onClicked.addListener(async function () {
     if (tabId != targetId || changedProps.status != 'complete') return;
 
     chrome.tabs.onUpdated.removeListener(listener);
-    chrome.tabs.sendMessage(tabId, { msg: 'screenshot', data: { screenshotUrl, htmlElement } });
+    chrome.tabs.sendMessage(tabId, { msg: 'screenshot', data: { screenshotUrls, screenshotFocusedUrls, htmlElement } });
   });
 
   const tab2 = await chrome.tabs.create({ url: viewTabUrl });
